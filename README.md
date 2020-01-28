0942192ccac1dcb1661fc8a52f054a9e9c2c2da6
Daniel Kim <danielskim25@g.ucla.edu>
2020-01-28 15:14:14 -0800

    first_commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Technical Report: Pneumonia Detection Image Classification
+---
+
+## Problem Statement:
+---
+The objective is to implement a Convolutional Neural Network (CNN) to classify whether patients have pneumonia or not based on x-ray images on their chest.WORK IN PROGRESS
+
+## Data:
+---
+The dataset consist of x-ray images of patients' chest. The files can be found from [Mendeley](https://data.mendeley.com/datasets/rscbjbr9sj/2).
+
+
+## Exploratory Data Analysis:
+---
+
+
+## Modeling:
+---
+CNN
+ResNet
+
+## Model Evaluation:
+---
+
+
+## Future Steps:
+---
+There is ALWAYS a need for future steps. 
---
+++ assets/tensor_data.py
@@ -0,0 +1,25 @@
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_directory = '../data/train/'
+test_directory = '../data/test/'
+
+train_img_gen = ImageDataGenerator(rescale=1./255, 
+                                   horizontal_flip = True,
+                                   rotation_range = 30,
+                                   zoom_range = .30)
+
+train_data = train_img_gen = train_img_gen.flow_from_directory(batch_size = 32,
+                                                               directory = train_directory,
+                                                               shuffle = True,
+                                                               target_size = (244,244),
+                                                               class_mode = 'binary'
+                                                              )
+test_img_gen = ImageDataGenerator(rescale=1./255)
+
+test_data = test_img_gen.flow_from_directory(batch_size = 32,
+                                             directory = test_directory,
+                                             shuffle = True,
+                                             target_size = (244,244),
+                                             class_mode = 'binary'
+                                                              )
